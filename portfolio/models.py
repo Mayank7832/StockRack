5be2c015f6dc13f81542f7d877757d271ffa80ec
Mayank Chopra <mayankchopra144@gmail.com>
2025-03-08 20:51:09 +0530

    mdc: Users model creation

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 class Stocks(models.Model):
@@ -15,3 +16,24 @@ class Stocks(models.Model):
             "StockName" : self.stockName,
             "StockPrice" : self.price,
         })
+    
+class User(models.Model):
+    userId = models.AutoField(db_column="usr_id",primary_key=True)
+    name = models.CharField(db_column="usr_name",max_length = 100)
+    emailId = models.EmailField(db_column="usr_email_id",unique=True)
+    password = models.CharField(db_column="usr_password",max_length=128)
+    createdOn = models.DateTimeField(db_column="createdOn",auto_now_add=True)
+    updatedOn = models.DateTimeField(db_column="updatedOn",auto_now = True)
+
+    def __str__(self):
+        return str({
+            "UserId" :self.userId,
+            "UserName" : self.name,
+            "EmailId" : self.emailId,
+            })
+    
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)  # Hash before saving
+        super().save(*args, **kwargs)
+
+    
