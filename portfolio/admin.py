9b2c3e1a4707550ad369046b174785ca6acc7ef6
Aayush-Chheda <aayush.chheda@somaiya.edu>
2025-03-09 16:11:03 +0530

    abc: Portfolio model creation

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from portfolio.models import User, Stock
+from portfolio.models import User, Stock, Portfolio
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Stock)
+admin.site.register(Portfolio)
--- portfolio/models.py
+++ portfolio/models.py
@@ -18,12 +18,12 @@ class Stock(models.Model):
         })
     
 class User(models.Model):
-    userId = models.AutoField(db_column="usr_id",primary_key=True)
-    name = models.CharField(db_column="usr_name",max_length = 100)
-    emailId = models.EmailField(db_column="usr_email_id",unique=True)
-    password = models.CharField(db_column="usr_password",max_length=128)
-    createdOn = models.DateTimeField(db_column="createdOn",auto_now_add=True)
-    updatedOn = models.DateTimeField(db_column="updatedOn",auto_now = True)
+    userId = models.AutoField(db_column="usr_id", primary_key=True)
+    name = models.CharField(db_column="usr_name", max_length = 100)
+    emailId = models.EmailField(db_column="usr_email_id", unique=True)
+    password = models.CharField(db_column="usr_password", max_length=128)
+    createdOn = models.DateTimeField(db_column="createdOn", auto_now_add=True)
+    updatedOn = models.DateTimeField(db_column="updatedOn", auto_now = True)
 
     def __str__(self):
         return str({
@@ -36,4 +36,25 @@ class User(models.Model):
         self.password = make_password(self.password)  # Hash before saving
         super().save(*args, **kwargs)
 
+class Portfolio(models.Model):
+    TRANSACTION_TYPE = [
+        ('B', 'Buy'),
+        ('S', 'Sell'),
+    ]
+
+    portfolioId = models.AutoField(db_column="pof_id", primary_key=True)
+    user = models.ForeignKey(User, db_column="pof_usr_id", on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, db_column="pof_stk_id", on_delete=models.CASCADE)
+    stockQty = models.IntegerField(db_column="pof_stk_qty")
+    transactionType = models.CharField(db_column="pof_tran_type", max_length=1, choices=TRANSACTION_TYPE)
+    transactionPrice = models.DecimalField(db_column="pof_tran_price", max_digits=9, decimal_places=2)
+    transactionDate = models.DateTimeField(db_column="pof_tran_date")
+    createdOn = models.DateTimeField(db_column="createdOn", auto_now_add=True)
+    updatedOn = models.DateTimeField(db_column="updatedOn", auto_now=True)
     
+    def __str__(self):
+        return str({
+            "PortfolioId": self.portfolioId,
+            "UserId": self.user.userId,
+            "StockId": self.stock.stockId,
+        })
