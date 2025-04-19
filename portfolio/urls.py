51b7c57cd3992751a1246bc62e5e8776684509b1
Mayank Chopra <mayankchopra144@gmail.com>
2025-04-19 16:08:46 +0530

    mdc: resolving errors

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,7 @@ app_name = 'portfolio'
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete_stock/<int:stockId>/', views.delete_stock, name='delete_stock'),
-    path('transactions', views.trasaction_view, name='transactions'),
+    path('transactions/<int:stock_id>/', views.transaction_view, name='transactions'),
     path("delete-transaction/<int:transaction_id>/", views.delete_transaction, name="delete_transaction"),
     path('add_trade/', views.add_trade, name='add_trade'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from userAuth.decorators import login_required_custom
 from django.views.decorators.http import require_POST
-from .models import Portfolio
-=======
+from django.http import JsonResponse
 from .forms import TradeForm
 from .models import Trade
->>>>>>> f030fc59f63ca5d2d26471b3ca998620bbad7757
-from .utils import PortfolioService
+from .utils import PortfolioService, validate_transaction_for_edit_or_delete, delete_subsequent_transactions
 
 # Create your views here.
 @login_required_custom
@@ -27,32 +25,47 @@ def delete_stock(request, stockId):
     Delete a stock from the portfolio.
     """
     if request.method == "POST":
-        txns = Trade.objects.filter(stock_id=stockId, user=request.myuser.userId)
+        txns = Trade.objects.filter(stock_id=stockId, user=request.myuser.user_id)
         txns.delete()
         messages.success(request, "Stock deleted successfully!")
     return redirect('portfolio:index')
 
 def add_trade(request):
+    print("Add Request received")
     if request.method == 'POST':
+        print("POST request received")
         form = TradeForm(request.POST)
-        if form.is_valid():
-            stock = form.cleaned_data['stock']
-            stockQty = form.cleaned_data['quantity']
-            transactionPrice = form.cleaned_data['trade_price']
-            transactionType = form.cleaned_data['direction']
-            transactionDate = form.cleaned_data['date']
+        userId = request.myuser.user_id
+        try: 
+            if form.is_valid():
+                stock = form.cleaned_data['stock']
+                stockQty = form.cleaned_data['quantity']
+                transactionPrice = form.cleaned_data['trade_price']
+                transactionType = form.cleaned_data['direction']
+                transactionDate = form.cleaned_data['date']
+                trades = []
 
-            portfolio = Trade(
-                user=request.myuser,
-                stock=stock,
-                quantity=stockQty,
-                trade_price=transactionPrice,
-                direction=transactionType,
-                date=transactionDate,
-            )
-            portfolio.save()
-            messages.success(request, "Trade added successfully!")
-            return redirect('portfolio:index')
+                trade = Trade(
+                    user=request.myuser,
+                    stock=stock,
+                    quantity=stockQty,
+                    trade_price=transactionPrice,
+                    direction=transactionType,
+                    date=transactionDate,
+                    quantity_before = PortfolioService(userId).GetQuantityBefore(stock.stock_id,transactionDate,transactionType)
+                )
+
+                trades.append(trade)
+
+                if transactionType == 'B' or validate_transaction_for_edit_or_delete(trades,'ADD'):
+                    trade.save()
+                    delete_subsequent_transactions(trade)
+
+                messages.success(request, "Trade added successfully!")
+                return redirect('portfolio:index')
+        except Exception as e:
+            print("Error:", str(e))
+            messages.error(request, f"Error: {str(e)}")
     else:
         form = TradeForm()
     return render(request, 'portfolio/add_trade.html', {'form': form})
@@ -70,26 +83,29 @@ def view_request(request):
     print("Session:", request.session)
     print("Logged-in user:", request.user)
 
-def trasaction_view(request):
-    #user_id = request.POST.get('user_id')
-    #stock_id = request.POST.get('stock_id')
+def transaction_view(request,stock_id):
 
-    user_id = 2;
-    stock_id = 5;
+    print("Transaction View Request received")
+    user_id = request.myuser.user_id
 
-    transactions = Portfolio.objects.filter(user_id=user_id, stock_id=stock_id).order_by('-transactionDate')
-    print(transactions);
-    print(transactions.first().portfolioId);
+    transactions = Trade.objects.filter(user_id=user_id, stock_id=stock_id).order_by('-date','-trade_id')
+    print(transactions)
     return render(request, 'portfolio/transaction.html', {'transactions': transactions})
 
 @require_POST
 def delete_transaction(request, transaction_id):
     try:
-        txn = Portfolio.objects.get(portfolioId=transaction_id)
-        
-        txn.delete()
+        trd = Trade.objects.get(trade_id=transaction_id)
+        trades = [trd]
+        if trd.direction == 'S' or validate_transaction_for_edit_or_delete(trades,'DELETE'): 
+            pk = trd.trade_id 
+            trd.delete()
+            trd.trade_id = pk
+            delete_subsequent_transactions(trd)
+        else:
+            raise Exception("Cannot delete as further transactions affected.")
         return JsonResponse({"success": True})
-    except Portfolio.DoesNotExist:
+    except Trade.DoesNotExist:
         return JsonResponse({"success": False, "error": "Transaction not found"})
     except Exception as e:
         return JsonResponse({"success": False, "error": str(e)})
